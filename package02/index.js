d22b0d629bb780be052d0132715fe7ef0a13d18c
Shaun Luttin <shaunluttin@gmail.com>
2019-05-29 19:12:35 -0700

    Create two demo packages

---
+++ package02/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var package01_1 = require("@shaunluttin/package01");
+package01_1.default();
---
+++ package02/index.ts
@@ -0,0 +1,3 @@
+import package01 from '@shaunluttin/package01';
+
+package01();
